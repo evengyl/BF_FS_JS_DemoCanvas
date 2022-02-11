0524c5727d35785389230ab653bf02421bb9d792
evengyl <dark.evengyl@gmail.com>
2022-02-11 09:42:28 +0100

    imp explain canvas rect

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ if(demo1.getContext)
     var ctx = demo1.getContext('2d')
 
     ctx.fillStyle = "red"
-    ctx.fillRect(50, 50, 200, 100)//x, y, w, h
+    ctx.fillRect(50, 50, 400, 150)//x, y, w, h
 
     ctx.fillStyle = "blue"
-    ctx.fillRect(70, 50, 100, 100)
-
+    ctx.fillRect(70, 50, 100, 150)
+    
     ctx.fillStyle = "green"
-    ctx.fillRect(190, 50, 30, 100)
+    ctx.fillRect(190, 50, 30, 150)
+    
+    ctx.clearRect(230, 60, 200, 130)
+
+    ctx.strokeStyle = "white"
+    ctx.strokeRect(250, 70, 150, 100)
+
+    ctx.fillRect(250, 70, 150, 100)
+
+
+    console.log(ctx)
 }
+
+
