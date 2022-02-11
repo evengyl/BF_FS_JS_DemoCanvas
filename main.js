8c15baedab4daaed9e8adb3f06762173a6a0f903
evengyl <dark.evengyl@gmail.com>
2022-02-11 11:53:06 +0100

    correction exos 1, 2

--- main.js
+++ main.js
@@ -16,11 +16,42 @@ if(demo1.getContext)
 
     ctx.strokeStyle = "white"
     ctx.strokeRect(250, 70, 150, 100)
-
+    
     ctx.fillRect(250, 70, 150, 100)
 
 
     console.log(ctx)
+}
+
+var exos1 = document.getElementById("exos1")
+if(exos1.getContext)
+{
+    var ctx = exos1.getContext('2d')
+
+    ctx.fillStyle = "black"
+    ctx.fillRect(50, 50, 200, 200)
+
+    ctx.clearRect(70, 70, 160, 160)
+
+    ctx.strokeStyle = "black"
+    ctx.strokeRect(90, 90, 120, 120)
+ 
+}
+
+
+var exos2 = document.getElementById("exos2")
+if(exos2.getContext)
+{
+    var ctx = exos2.getContext("2d")
+
+    ctx.strokeStyle = "grey"
+    ctx.strokeRect(50, 50, 400, 250)
+
+    ctx.fillStyle = "red"
+    ctx.fillRect(70, 70, 360, 210)
+
+    ctx.clearRect(175, 155, 150, 40) //400-150 = 250 / 2 = 125 + 50 = 175 //250 - 40 = 210 /2 = 105 + 50 =155
+    ctx.clearRect(230,100,40,150) // 400 -40 = 360 /2 = 180 +50 = 230 // 250 - 150 = 100 /2 = 50 + 50 = 100
 }
 
 
