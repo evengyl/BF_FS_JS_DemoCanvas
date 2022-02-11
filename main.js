14ad2b34e00d576238a9ab42988a6e96c9cdd08e
evengyl <dark.evengyl@gmail.com>
2022-02-11 13:59:04 +0100

    imp demo to arc & arcto

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ if(demo1.getContext)
     console.log(ctx)
 }
 
+
+
 var exos1 = document.getElementById("exos1")
 if(exos1.getContext)
 {
@@ -37,6 +39,7 @@ if(exos1.getContext)
     ctx.strokeRect(90, 90, 120, 120)
  
 }
+
 
 
 var exos2 = document.getElementById("exos2")
@@ -55,3 +58,112 @@ if(exos2.getContext)
 }
 
 
+
+var demo2 = document.getElementById("demo2")
+if(demo2.getContext)
+{
+    var ctx = demo2.getContext("2d")
+
+    ctx.beginPath()
+
+    ctx.moveTo(50,50)
+    ctx.lineTo(300, 50)
+    ctx.lineTo(50, 300)
+    ctx.lineTo(50, 50)
+
+    ctx.fill()
+    ctx.closePath()
+
+
+
+    ctx.beginPath()
+
+    ctx.moveTo(350, 100)
+    ctx.lineTo(350, 350)
+    ctx.lineTo(100, 350)
+    ctx.lineTo(350, 100)
+
+    ctx.stroke()
+    ctx.closePath()
+}
+
+
+
+var demo3 = document.getElementById("demo3")
+if(demo3.getContext)
+{
+    var ctx = demo3.getContext("2d")
+
+    ctx.beginPath()
+
+    ctx.moveTo(50, 50)
+    // ctx.lineTo(350, 50)
+    // ctx.lineTo(350, 350)
+    // ctx.lineTo(50, 350)
+    // ctx.lineTo(50, 50)
+
+    ctx.rect(50, 50, 300, 300)
+
+    ctx.strokeStyle = "green"
+    ctx.stroke()
+    ctx.closePath()
+}
+
+
+var demo4 = document.getElementById("demo4")
+if(demo4.getContext)
+{
+    var ctx = demo4.getContext("2d")
+
+    ctx.beginPath()
+    ctx.moveTo(350, 250)
+    ctx.arc(250, 250, 100, 0, Math.PI*2)
+
+    ctx.moveTo(230, 210)
+    ctx.arc(210, 210, 20, 0, Math.PI*2)
+
+    ctx.moveTo(310, 210)
+    ctx.arc(290, 210, 20, 0, Math.PI*2)
+
+    ctx.moveTo(330, 250)
+    ctx.arc(250, 250, 80, 0, Math.PI)
+
+    ctx.stroke()
+
+    ctx.closePath()
+}
+
+
+var demo5 = document.getElementById("demo5")
+if(demo5.getContext)
+{
+    var ctx = demo5.getContext("2d")
+
+
+    ctx.beginPath()
+    ctx.moveTo(55, 50)
+    ctx.fillStyle = "red"
+    ctx.arc(50, 50, 5, 0, Math.PI*2)
+    ctx.fill()
+    
+    ctx.moveTo(355, 350)
+    ctx.arc(350, 350, 5, 0, Math.PI*2)
+    ctx.fill()
+
+    ctx.moveTo(355, 50)
+    ctx.arc(350, 50, 5, 0, Math.PI*2)
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.lineWidth = "10"
+    ctx.moveTo(50, 50)
+    ctx.arcTo(350, 50, 350, 350, 100)
+    ctx.lineTo(350, 350)
+    
+
+    ctx.stroke()
+    ctx.closePath()
+
+
+}
